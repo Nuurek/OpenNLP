2488fa2f7637e96440ae30dee1119c57c08cc2f7
Jakub Nurski <nurskijakub@gmail.com>
2018-10-10 22:20:01 +0200

    Used sentence detection.

--- src/OpenNLP.java
+++ src/OpenNLP.java
@@ -1,34 +1,40 @@
 import opennlp.tools.langdetect.Language;
 import opennlp.tools.langdetect.LanguageDetectorME;
 import opennlp.tools.langdetect.LanguageDetectorModel;
+import opennlp.tools.sentdetect.SentenceDetector;
+import opennlp.tools.sentdetect.SentenceDetectorME;
+import opennlp.tools.sentdetect.SentenceModel;
+import opennlp.tools.tokenize.TokenizerME;
+import opennlp.tools.tokenize.TokenizerModel;
+import opennlp.tools.util.Span;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 
 @SuppressWarnings("SpellCheckingInspection")
 public class OpenNLP {
 
     private static String LANG_DETECT_MODEL = "resources/models/langdetect-183.bin";
-    public static String TOKENIZER_MODEL = "resources/models/en-token.bin";
-    public static String SENTENCE_MODEL = "resources/models/en-sent.bin";
-    public static String POS_MODEL = "resources/models/en-pos-maxent.bin";
-    public static String CHUNKER_MODEL = "resources/models/en-chunker.bin";
-    public static String LEMMATIZER_DICT = "resources/models/en-lemmatizer.dict";
-    public static String NAME_MODEL = "resources/models/en-ner-person.bin";
-    public static String ENTITY_XXX_MODEL = "resources/models/en-ner-xyz.bin";
+    private static String ENGLISH_TOKENIZER_MODEL = "resources/models/en-token.bin";
+    private static String GERMAN_TOKENIZER_MODEL = "resources/models/de-token.bin";
+    private static String SENTENCE_MODEL = "resources/models/en-sent.bin";
+    private static String POS_MODEL = "resources/models/en-pos-maxent.bin";
+    private static String CHUNKER_MODEL = "resources/models/en-chunker.bin";
+    private static String LEMMATIZER_DICT = "resources/models/en-lemmatizer.dict";
+    private static String NAME_MODEL = "resources/models/en-ner-person.bin";
+    private static String ENTITY_XXX_MODEL = "resources/models/en-ner-xyz.bin";
 
-	public static void main(String[] args) throws IOException
-    {
+	public static void main(String[] args) throws IOException {
 		OpenNLP openNLP = new OpenNLP();
 		openNLP.run();
 	}
 
-	private void run() throws IOException
-    {
+	private void run() throws IOException {
 
 		languageDetection();
-		// tokenization();
-        // sentenceDetection();
+        tokenization();
+        sentenceDetection();
 		// posTagging();
 		// lemmatization();
 		// stemming();
@@ -36,28 +42,29 @@ public class OpenNLP {
 		// nameFinding();
 	}
 
-	private void languageDetection() throws IOException
-    {
+	private void languageDetection() throws IOException {
+        System.out.println("Language detection");
+
 		File modelFile = new File(LANG_DETECT_MODEL);
 		LanguageDetectorModel model = new LanguageDetectorModel(modelFile);
         LanguageDetectorME detector = new LanguageDetectorME(model);
 
 		String[] texts = {
-                "cats",
-                "Many cats like milk because in some ways it reminds them of their mother's milk.",
-                "The two things are not really related. Many cats like milk because in "
-                + "some ways it reminds them of their mother's milk.",
-                "The two things are not really related. Many cats like milk because in some ways it reminds them of their mother's milk. "
-                + "It is rich in fat and protein. They like the taste. They like the consistency . "
-                + "The issue as far as it being bad for them is the fact that cats often have difficulty digesting milk and so it may give them "
-                + "digestive upset like diarrhea, bloating and gas. After all, cow's milk is meant for baby calves, not cats. "
-                + "It is a fortunate quirk of nature that human digestive systems can also digest cow's milk. But humans and cats are not cows.",
-                "Many cats like milk because in some ways it reminds them of their "
-                + "mother's milk. Le lait n'est pas forc�ment mauvais pour les chats",
-                "Many cats like milk because in some ways it reminds them of their "
-                + "mother's milk. Le lait n'est pas forc�ment mauvais pour les chats. "
-                + "Der Normalfall ist allerdings der, dass Salonl�wen Milch weder brauchen "
-                + "noch gut verdauen k�nnen."
+            "cats",
+            "Many cats like milk because in some ways it reminds them of their mother's milk.",
+            "The two things are not really related. Many cats like milk because in "
+            + "some ways it reminds them of their mother's milk.",
+            "The two things are not really related. Many cats like milk because in some ways it reminds them of their mother's milk. "
+            + "It is rich in fat and protein. They like the taste. They like the consistency . "
+            + "The issue as far as it being bad for them is the fact that cats often have difficulty digesting milk and so it may give them "
+            + "digestive upset like diarrhea, bloating and gas. After all, cow's milk is meant for baby calves, not cats. "
+            + "It is a fortunate quirk of nature that human digestive systems can also digest cow's milk. But humans and cats are not cows.",
+            "Many cats like milk because in some ways it reminds them of their "
+            + "mother's milk. Le lait n'est pas forc�ment mauvais pour les chats",
+            "Many cats like milk because in some ways it reminds them of their "
+            + "mother's milk. Le lait n'est pas forc�ment mauvais pour les chats. "
+            + "Der Normalfall ist allerdings der, dass Salonl�wen Milch weder brauchen "
+            + "noch gut verdauen k�nnen."
         };
         for (String text: texts) {
             System.out.println(text);
@@ -67,33 +74,61 @@ public class OpenNLP {
         }
 	}
 
-	private void tokenization() throws IOException
-    {
-		String text = "";
-
-		text = "Since cats were venerated in ancient Egypt, they were commonly believed to have been domesticated there, "
-				+ "but there may have been instances of domestication as early as the Neolithic from around 9500 years ago (7500 BC).";
-		/*text = "Since cats were venerated in ancient Egypt, they were commonly believed to have been domesticated there, "
-				+ "but there may have been instances of domestication as early as the Neolithic from around 9,500 years ago (7,500 BC).";
-		text = "Since cats were venerated in ancient Egypt, they were commonly believed to have been domesticated there, "
-		 + "but there may have been instances of domestication as early as the Neolithic from around 9 500 years ago ( 7 500 BC).";*/
-
+	private void tokenization() throws IOException {
+	    tokenizeByLanguage(ENGLISH_TOKENIZER_MODEL);
+	    tokenizeByLanguage(GERMAN_TOKENIZER_MODEL);
 	}
 
-	private void sentenceDetection() throws IOException
-    {
-		String text = "";
-		text = "Hi. How are you? Welcome to OpenNLP. "
-				+ "We provide multiple built-in methods for Natural Language Processing.";
-		/*text = "Hi. How are you?! Welcome to OpenNLP? "
-				+ "We provide multiple built-in methods for Natural Language Processing.";
-		text = "Hi. How are you? Welcome to OpenNLP.?? "
-				+ "We provide multiple . built-in methods for Natural Language Processing.";
-		text = "The interrobang, also known as the interabang (often represented by ?! or !?), "
-				+ "is a nonstandard punctuation mark used in various written languages. "
-				+ "It is intended to combine the functions of the question mark (?), or interrogative point, "
-				+ "and the exclamation mark (!), or exclamation point, known in the jargon of printers and programmers as a \"bang\". ";*/
+	private void tokenizeByLanguage(String modelFilePath) throws IOException {
+        System.out.printf("Tokenization %s\n", modelFilePath);
 
+        File modelFile = new File(modelFilePath);
+        TokenizerModel tokenizerModel = new TokenizerModel(modelFile);
+        TokenizerME tokenizer = new TokenizerME(tokenizerModel);
+
+        String[] texts = {
+                "Since cats were venerated in ancient Egypt, they were commonly believed to have been domesticated there, "
+                        + "but there may have been instances of domestication as early as the Neolithic from around 9500 years ago (7500 BC).",
+                "Since cats were venerated in ancient Egypt, they were commonly believed to have been domesticated there, "
+                        + "but there may have been instances of domestication as early as the Neolithic from around 9,500 years ago (7,500 BC).",
+                "Since cats were venerated in ancient Egypt, they were commonly believed to have been domesticated there, "
+                        + "but there may have been instances of domestication as early as the Neolithic from around 9 500 years ago ( 7 500 BC)."
+        };
+
+        for (String text: texts) {
+            System.out.println(text);
+
+            tokenizer.tokenize(text);
+            double[] tokenProbabilities = tokenizer.getTokenProbabilities();
+            System.out.println(Arrays.toString(tokenProbabilities));
+        }
+    }
+
+	private void sentenceDetection() throws IOException {
+        System.out.println("Sentence detection");
+
+        File modelFile = new File(SENTENCE_MODEL);
+        SentenceModel sentenceModel = new SentenceModel(modelFile);
+        SentenceDetectorME sentenceDetector = new SentenceDetectorME(sentenceModel);
+
+        String[] texts = {
+            "Hi. How are you? Welcome to OpenNLP. "
+            + "We provide multiple built-in methods for Natural Language Processing.",
+            "Hi. How are you? Welcome to OpenNLP.?? "
+            + "We provide multiple . built-in methods for Natural Language Processing.",
+            "The interrobang, also known as the interabang (often represented by ?! or !?), "
+            + "is a nonstandard punctuation mark used in various written languages. "
+            + "It is intended to combine the functions of the question mark (?), or interrogative point, "
+            + "and the exclamation mark (!), or exclamation point, known in the jargon of printers and programmers as a \"bang\". "
+        };
+
+        for (String text: texts) {
+            System.out.println(text);
+
+            sentenceDetector.sentDetect(text);
+            double[] sentenceProbabilities = sentenceDetector.getSentenceProbabilities();
+            System.out.println(Arrays.toString(sentenceProbabilities));
+        }
 	}
 
 	private void posTagging() throws IOException {
